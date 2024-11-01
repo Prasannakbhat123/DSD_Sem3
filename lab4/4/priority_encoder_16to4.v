be698557d6514339b75a7aea0aa1df388c04f4f4
Prasanna <139959015+Prasannakbhat123@users.noreply.github.com>
2024-11-01 21:42:01 +0530

    Update priority_encoder_16to4.v

--- lab4/4/priority_encoder_16to4.v
+++ lab4/4/priority_encoder_16to4.v
@@ -1,20 +1,20 @@
-module priority_encoder_16to4(in,en,out);
-input [15:0]in;
-input en;
-output reg [3:0]out;
-integer i;
-always @(in,en)
-begin
-if(en==1) begin
-for(i=15;i>=0;i=i-1) begin
-if(in[i]==1) begin
-out=i;
-break;
-end
-end
-end
-else
-begin
-out=4'b0000;
-end
+module priority_encoder_16to4(in, en, out);
+    input [15:0] in;
+    input en;
+    output reg [3:0] out;
+    integer i;
+
+    always @(in, en) begin
+        if (en == 1) begin
+            out = 4'b0000; // Initialize out to 0 in case no inputs are high
+            for (i = 15; i >= 0; i = i - 1) begin
+                if (in[i] == 1) begin
+                    out = i;
+                    disable for; // Exit the for loop once the highest priority bit is found
+                end
+            end
+        end else begin
+            out = 4'b0000; // Output default value when enable is low
+        end
+    end
 endmodule
